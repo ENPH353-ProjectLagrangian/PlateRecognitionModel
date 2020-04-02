61fa71742ab525b98920cdf292101499d0800b49
jbellevi <juliebelleville@protonmail.com>
2020-04-01 21:52:58 -0700

    everything but plates masked out

--- Preprocessing/IsolatingPlates/plate_isolator_colour.py
+++ Preprocessing/IsolatingPlates/plate_isolator_colour.py
@@ -4,6 +4,10 @@ import cv2
 import numpy as np
 
 
+def _contour_area_tuple(c):
+    return cv2.contourArea(c[0])
+
+
 class PlateIsolatorColour:
     """
     The goal of this module is to pick out parking
@@ -36,16 +40,15 @@ class PlateIsolatorColour:
         """
         Returns plates in order: parking, license, or None if no plates found
         """
-        car_mask, car_colour = self.get_car_mask(img)
+        hsb = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        car_mask, car_colour = self.get_car_mask(hsb)
         if car_mask is None:
             print("no plate_found")
             return None
-        parking, license = self.get_plate_contours(img, car_mask, car_colour)
+        parking, license = self.get_plate_contours(hsb, car_mask, car_colour)
 
     def get_car_mask(self, img, duration=1000):
         bound_num = 0
-
-        hsb = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         mask = [None, None, None]
 
@@ -64,7 +67,7 @@ class PlateIsolatorColour:
                 else:
                     title = "yellow"
 
-            mask[bound_num] = cv2.inRange(hsb, lower, upper)
+            mask[bound_num] = cv2.inRange(img, lower, upper)
 
             # if self.testing:
             #     output = cv2.bitwise_and(img, img, mask=mask[bound_num])
@@ -96,10 +99,19 @@ class PlateIsolatorColour:
         print(img.shape)
         print(mask.shape)
         img = cv2.bitwise_and(img, img, mask=mask)
-        if self.testing:
-            cv2.imshow("mask", mask)
+
+        (lower, upper) = self.colour_bounds[colour]
+        # create numpy arrays from colour boundaries
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
+        colour_mask = cv2.inRange(img, lower, upper)
+        colour_mask = cv2.bitwise_not(colour_mask)
+        img = cv2.bitwise_and(img, img, mask=colour_mask)
+
+        if self.testing:            
             cv2.imshow("masked image", img)
             cv2.waitKey(2000)
+
         return 1, 2
 
     def car_contour(self, mask):
@@ -113,18 +125,19 @@ class PlateIsolatorColour:
         guestimate area: experimentally determined
         """
         MIN_AREA = (int)(0.75 * mask[0].shape[1] / 6 * mask[0].shape[0] / 4)
-        good_contours = [c for c in (contours0 + contours1 + contours2)
-                         if cv2.contourArea(c) > MIN_AREA]
-        list.sort(good_contours, key=cv2.contourArea)
+        
+        cont_0 = [(c, 0) for c in contours0 if cv2.contourArea(c) > MIN_AREA]
+        cont_1 = [(c, 1) for c in contours1 if cv2.contourArea(c) > MIN_AREA]
+        cont_2 = [(c, 2) for c in contours2 if cv2.contourArea(c) > MIN_AREA]
+
+        good_contours = [c for c in (cont_0 + cont_1 + cont_2)
+                         if cv2.contourArea(c[0]) > MIN_AREA]
+
+        list.sort(good_contours, key=_contour_area_tuple)
         if (len(good_contours) == 0):
-            return None
+            return None, None
+
         if self.testing:
             print(good_contours[0])
 
-        selected_mask = "green"
-        if (good_contours[0] in contours1):
-            selected_mask = "blue"
-        elif (good_contours[0] in contours2):
-            selected_mask = "yellow"
-
-        return selected_mask, good_contours[0]
+        return good_contours[0][1], good_contours[0][0]
